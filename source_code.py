1c8036d1aa39e23cd90cb2b7894223836f2fa411
Ritesh2k24 <162950856+Ritesh2k24@users.noreply.github.com>
2025-02-24 12:09:31 +0530

    Update source_code.py

--- source_code.py
+++ source_code.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "This is the first Assignment for Virtualization and Cloud Computing"
+    return "This is the second Assignment for Virtualization and Cloud Computing"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
